69c4ebb4e3eefd400170e3a410287c4ffd2c7c9d
Dustywade <92659215+Dustywade@users.noreply.github.com>
2024-07-12 22:58:01 -0400

    Renamed - Updated README - Cleaned up

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fresh-kali
- automated script for updating kali
+I got bored of doing the same thing over and over again. Not reinventing the wheel, just wanted to customize it and see if I could do it.
+Feel free to update this script as you like. If you come across any issues let me know. If you think of someway I can make this script better let me know!
+
--- freshkali.sh
+++ freshkali.sh
@@ -22,7 +22,9 @@ check_root
 
 echo -e "${YELLOW}\nMOST of this script is automated.."
 echo -e "The only time you might have to interact is during kali "update / upgrade""
-echo -e "Once that completes it should be hands free${NC}"
+echo -e "Once that completes it should be hands free"
+echo -e "Depending on how lazy you are with updating"
+echo -e "...this could take a little while, grab a coffee!${NC}"
 sleep 5
 
 # Fix kali mirror - issue that has popped up randomly
@@ -31,15 +33,15 @@ echo "deb http://kali.download/kali kali-rolling main contrib non-free non-free-
 
 # Update and upgrade the system
 # libpcap-dev needed for PD naabu
-echo -e "${YELLOW}\nUpdating and upgrading..${NC}"
+echo -e "${YELLOW}\n\nUpdating and upgrading..${NC}"
 #sudo apt install libpcap-dev -y
 sudo apt update &>/dev/null && sudo apt upgrade -y --fix-missing
 wait
 
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}\nKali update successful${NC}"
+    echo -e "${GREEN}\n\nKali update successful${NC}"
 else
-    echo -e "${RED}\nKali update failed${NC}"
+    echo -e "${RED}\n\nKali update failed${NC}"
     exit 1
 fi
 
@@ -48,24 +50,26 @@ sudo apt install docker.io docker-compose -y
 wait
 
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}\nDocker install successful${NC}"
+    echo -e "${GREEN}\n\nDocker install successful${NC}"
 else
-    echo -e "${RED}\nDocker install failed${NC}"
+    echo -e "${RED}\n\nDocker install failed${NC}"
     exit 1
 fi
 
 # Install pimpmykali if not already installed
 if [ -f pimpmykali/pimpmykali.sh ]; then
-    echo -e "${RED}\nPimpmykali already installed!${NC}"
+    echo -e "${RED}\n\nPimpmykali already installed!${NC}"
     sleep 2
 else
-    echo -e "${YELLOW}\nDownloading pimpmykali...${NC}"
+    echo -e "${YELLOW}\n\nDownloading pimpmykali...${NC}"
     git clone https://github.com/Dewalt-arch/pimpmykali.git &>/dev/null &
     wait
+    sleep 2
 
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}\nPimpmykali download complete!${NC}"
+        echo -e "${GREEN}\n\nPimpmykali download complete!${NC}"
         chmod +x pimpmykali/pimpmykali.sh
+        sleep 2
 
         # Interactive prompt using expect
         expect <<EOF
@@ -80,37 +84,39 @@ else
 EOF
         # Check if expect succeeded
         if [ $? -eq 0 ]; then
-            echo -e "${GREEN}\nPimpmykali installation and configuration complete!${NC}"
+            echo -e "${GREEN}\n\nPimpmykali installation and configuration complete!${NC}"
         else
-            echo -e "${RED}\nSomething went wrong during pimpmykali installation!${NC}"
+            echo -e "${RED}\n\nSomething went wrong during pimpmykali installation!${NC}"
             exit 1
         fi
     else
-        echo -e "${RED}\nPimpmykali download failed!${NC}"
+        echo -e "${RED}\n\nPimpmykali download failed!${NC}"
         exit 1
     fi
 fi
 
 # Install additional tools
 if [ -f /home/$SUDO_USER/go/bin/pdtm ]; then
-    echo -e "${RED}\nPD tool manager already installed!${NC}"
+    echo -e "${RED}\n\nPD tool manager already installed!${NC}"
     exit 1
 else
-    echo -e "${YELLOW}\nInstalling PD tool manager..${NC}"
+    echo -e "${YELLOW}\n\nInstalling PD tool manager..${NC}"
     sudo -u "$SUDO_USER" go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+    wait
+    
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}PD tool manager download complete!${NC}"
+        echo -e "${GREEN}\n\nPD tool manager download complete!${NC}"
         sudo -u "$SUDO_USER" /home/$SUDO_USER/go/bin/pdtm
         sudo -iu "$SUDO_USER" zsh -c "source ~/.zshrc"
         sudo -u "$SUDO_USER" /home/$SUDO_USER/go/bin/pdtm --install-all
         if [ $? -eq 0 ]; then
-            echo -e "${GREEN}\nPD tool manager installation complete!${NC}"
+            echo -e "${GREEN}\n\nPD tool manager installation complete!${NC}"
         else
-            echo -e "${RED}\nSomething went wrong during PD tool manager!${NC}"
+            echo -e "${RED}\n\nSomething went wrong during PD tool manager!${NC}"
             exit 1
         fi
     else
-        echo -e "${RED}\nPD tool manager download failed${NC}"
+        echo -e "${RED}\n\nPD tool manager download failed${NC}"
         exit 1
     fi
 fi
@@ -119,11 +125,13 @@ fi
 sudo mkdir /opt/Bloodhound
 cd /opt/Bloodhound
 sudo curl -Lo docker-compose.yml https://ghst.ly/getbhce
+wait
+
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}\nBloodhound download successful${NC}"
+    echo -e "${GREEN}\n\nBloodhound download successful${NC}"
 else
-    echo -e "${RED}\nBloodhound download failed${NC}"
+    echo -e "${RED}\n\nBloodhound download failed${NC}"
     exit 1
 fi
 
-echo -e "${GREEN}\nSetup complete!${NC}"
+echo -e "${GREEN}\n\nSetup complete!${NC}"
